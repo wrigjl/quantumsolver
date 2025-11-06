53cb4317a0387e656666aa5824a3bc9aee3b95c2
Jason Wright <jason@thought.net>
2025-11-06 08:48:12 -0700

    remove cruft

--- Deutsch.py
+++
@@ -1,6 +0,0 @@
-
-'''TODO: Implement a solver for the Deutsch problem'''
-
-def solve(text):
-    """Given a problem instance in json format, return a json result"""
-    return text
--- testdeutsch.py
+++
@@ -1,9 +0,0 @@
-
-'''This just makes a requests and prints the resulting json'''
-
-import requests
-
-
-foo = {"key1": "value1", "key2": "value2"}
-req = requests.post(url="http://127.0.0.1:5000/deutsch", json=foo)
-print(req.json())
