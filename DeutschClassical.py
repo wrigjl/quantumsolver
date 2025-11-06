321806803c1c25418cf629331fcb087de334f470
Jason Wright <jason@thought.net>
2025-11-06 10:02:39 -0700

    fix up some lint warnings

--- DeutschClassical.py
+++ DeutschClassical.py
@@ -1,12 +1,10 @@
 
 '''This is a classical solver for the Deutsch problem'''
 
-def solve(data):
-    """Given a problem instance in json format, return a json result"""
-    
-    """We expect the incoming json schema to be a list of length two.
-       Each entry is either true or false. The first number represents
-       f(false), and the second represents f(true)"""
+def solve(data) -> dict:
+    ''' We expect the incoming json schema to be a list of length two.
+    # Each entry is either true or false. The first number represents
+    # f(false), and the second represents f(true)'''
     if data[0] == data[1]:
         result = {"answer": "constant"}
     else:
