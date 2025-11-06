e4fc9f8384620e1a6ae4a8223457d2ca01c245db
Jason Wright <jason@thought.net>
2025-11-06 10:27:26 -0700

    pretty

--- DeutschClassical.py
+++ DeutschClassical.py
@@ -3,10 +3,8 @@
 
 def solve(data) -> dict:
     ''' We expect the incoming json schema to be a list of length two.
-    # Each entry is either true or false. The first number represents
-    # f(false), and the second represents f(true)'''
-    if data[0] == data[1]:
-        result = {"answer": "constant"}
-    else:
-        result = {"answer": "balanced"}
-    return result
+        Each entry is either true or false. The first number represents
+        f(false), and the second represents f(true)
+    '''
+    result = "constant" if data[0] == data[1] else "balanced"
+    return {'answer': result}
